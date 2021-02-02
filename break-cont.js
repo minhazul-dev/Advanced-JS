5552fe828ee864e544a19b0b77752ca6723a226a
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2021-02-02 22:20:44 +0600

    All done

---
+++ break-cont.js
@@ -0,0 +1,29 @@
+const nums = [1,2,-3,4,-5,6,7,-8];
+for (let i=0; i<nums.length; i++){
+    
+    if (nums[i]>4) {
+        break;
+        
+    }
+    // console.log(nums[i]);
+
+}
+
+for (let i=0; i<nums.length; i++){
+    
+    if (nums[i]<0) {
+        continue;
+        
+    }
+    console.log(nums[i]);
+}
+
+function doSomething(x, y = 4){
+
+    console.log(y);
+    
+    }
+    
+    doSomething(3,2);
+    
+    
---
+++ scope.js
@@ -0,0 +1,15 @@
+
+let bonus = 15;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    if (result>10) {
+        const mood = "happy";
+        console.log(mood);
+        
+    }
+    return result;
+    
+}
+const output =sum(10,5);
+// console.log(output);
